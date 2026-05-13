d2900a43c123757214b7d55de3e64b0c6fa76663
KasibM <syuriix@yahoo.ca>
2026-05-13 09:42:19 -0400

    numbers and variables scripts moved to new files

--- Week2/js/numbers.js
+++ Week2/js/numbers.js
@@ -1,40 +1,39 @@
-<script>
-        /* STEP 1: Number types (integer, float, and double)
-        declare and initialize an integer and a float, then in the console, reveal the number type with typeof(varName) */
-        let numInt = 1;
-        let numFloat = 1.01;
-        console.log("numInt Type: " + typeof(numInt));
-        console.log("numFloat Type: "+ typeof(numFloat));
-        // Note - there are also different number systems: binary, octal, and hexadecimal
 
-        /* STEP 2: Arithmetic operators
-        + (addition), - (subtraction), * (multiplication), / (division), % (remainder or modulo), ** (exponent) */
+/* STEP 1: Number types (integer, float, and double)
+declare and initialize an integer and a float, then in the console, reveal the number type with typeof(varName) */
+let numInt = 1;
+let numFloat = 1.01;
+console.log("numInt Type: " + typeof(numInt));
+console.log("numFloat Type: "+ typeof(numFloat));
+// Note - there are also different number systems: binary, octal, and hexadecimal
 
-
-        // Try declaring and initializing a couple of variables as numbers
+/* STEP 2: Arithmetic operators
++ (addition), - (subtraction), * (multiplication), / (division), % (remainder or modulo), ** (exponent) */
 
 
-        /* OR… num1 * num2 / 8 + 2 - 0.5; */
-
-        /* Keep in mind operator precedence, just like you were taught in math class (/, *, +, -)
-        num1 + num2 * 5 - 25 ... is not 575 ... it is 215 */
-        
-        /* If you want to avoid this precedence, use parenthesis
-        (num1 + num2) * 5 - 25 */
-
-        /* STEP 3: Increment and decrement operators
-        ++ and -- are mostly found inside loops to move a counter variable to the next value when needed, either up or down. */
+// Try declaring and initializing a couple of variables as numbers
 
 
-        // Note 1 - you cannot increment/decrement a number directly
-        // Note 2 - typing num++ or num-- in the console will output the number before incrementing or decrmenting - try ++num or --num, instead
+/* OR… num1 * num2 / 8 + 2 - 0.5; */
 
-        /* STEP 4: Assignment Operators
-        = (assignment), += (addition assignment), -= (subtraction assignment), *= (multiplication assignment), /= (division assignment) */
+/* Keep in mind operator precedence, just like you were taught in math class (/, *, +, -)
+num1 + num2 * 5 - 25 ... is not 575 ... it is 215 */
+
+/* If you want to avoid this precedence, use parenthesis
+(num1 + num2) * 5 - 25 */
+
+/* STEP 3: Increment and decrement operators
+++ and -- are mostly found inside loops to move a counter variable to the next value when needed, either up or down. */
 
 
-        /* STEP 5: Comparison Operators
-        ===	(strict equality), !==	(strict-non-equality), == (equality), < (less than), > (greater than), <= (less than or equal to), >=	(greater than or equal to) */
+// Note 1 - you cannot increment/decrement a number directly
+// Note 2 - typing num++ or num-- in the console will output the number before incrementing or decrmenting - try ++num or --num, instead
+
+/* STEP 4: Assignment Operators
+= (assignment), += (addition assignment), -= (subtraction assignment), *= (multiplication assignment), /= (division assignment) */
 
 
-    </script>
+/* STEP 5: Comparison Operators
+===	(strict equality), !==	(strict-non-equality), == (equality), < (less than), > (greater than), <= (less than or equal to), >=	(greater than or equal to) */
+
+
